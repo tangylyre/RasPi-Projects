b4be78ef233cf29998191670edcb09b1ab29346d
tangylyre <tangylyre@gmail.com>
2020-08-19 15:45:11 -0700

    add tswitch

---
+++ 3 Pin/tswitch.py
@@ -0,0 +1,9 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+while True:
+    GPIO.output(18, True)
+    time.sleep(30)
+    GPIO.output(18, False)
+    time.sleep(30)
